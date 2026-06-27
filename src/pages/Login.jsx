67f12021d81118ddb1c4ae14d2c7e5fc2137ec08
Manikandan-1923 <maniathitiyanz@gmail.com>
2026-06-27 23:23:47 +0530

    Completed React Login with JWT

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,63 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    console.log(email);
-    console.log(password);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/users/login",
+        {
+          email,
+          password,
+        }
+      );
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login Successful ✅");
+
+      navigate("/profile");
+
+    } catch (error) {
+      alert(error.response?.data?.message || "Login Failed");
+    }
   };
 
   return (
     <div className="login-container">
       <div className="login-box">
+
         <h1>Login 🔐</h1>
 
-      <input
-        type="email"
-        placeholder="Enter Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <br /><br />
+        <br /><br />
 
-      <input
-        type="password"
-        placeholder="Enter Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <br /><br />
+        <br /><br />
 
-      <button onClick={handleLogin}>
-        Login
-      </button>
-    </div>
+        <button onClick={handleLogin}>
+          Login
+        </button>
+
+      </div>
     </div>
   );
 }
