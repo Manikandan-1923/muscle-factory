32250dc2835517b01d6f5f9117e0b4184cc039cf
Manikandan-1923 <maniathitiyanz@gmail.com>
2026-07-07 11:14:53 +0530

    Implement JWT authentication and profile API

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Profile.css";
 
@@ -6,12 +7,43 @@ function Profile() {
 
   const [isEditing, setIsEditing] = useState(false);
 
-  const [name, setName] = useState("Manikandan");
-  const [email, setEmail] = useState("maniathitiyanz@gmail.com");
-  const [goal, setGoal] = useState("Muscle Gain");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [goal, setGoal] = useState("");
   const [height, setHeight] = useState("170");
   const [weight, setWeight] = useState("70");
   const navigate = useNavigate();
+
+  useEffect(() => {
+  const fetchProfile = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await axios.get(
+        "http://localhost:5000/api/users/profile",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setName(res.data.name);
+      setEmail(res.data.email);
+      setGoal(res.data.goal);
+
+    } catch (error) {
+      console.log(error);
+
+      if (error.response?.status === 401) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      }
+    }
+  };
+
+  fetchProfile();
+}, [navigate]);
 
 
   const handleLogout = () => {
