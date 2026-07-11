2882c81efc0b637f42b06de7c62bc50ad682c7ed
Manikandan-1923 <maniathitiyanz@gmail.com>
2026-07-11 13:08:08 +0530

    Implement workout CRUD frontend integration

--- src/pages/Workouts.jsx
+++ src/pages/Workouts.jsx
@@ -1,31 +1,181 @@
-import { useState } from "react";
-import WorkoutCard from "../components/WorkoutCard";
-import workouts from "../data/workouts";
-import "./Workouts.css";
+import { useState , useEffect} from "react";
+import axios from "axios";
 
 function Workouts() {
-  const [search, setSearch] = useState("");
+  const [exerciseName, setExerciseName] = useState("");
+  const [muscleGroup, setMuscleGroup] = useState("");
+  const [sets, setSets] = useState("");
+  const [reps, setReps] = useState("");
+  const [duration, setDuration] = useState("");
+  const [workouts, setWorkouts] = useState([]);
+  
+const fetchWorkouts = async () => {
+  try {
+    const token = localStorage.getItem("token");
 
-  const filteredWorkouts = workouts.filter((workout) =>
-    workout.title.toLowerCase().includes(search.toLowerCase())
-  );
+    const res = await axios.get(
+      "http://localhost:5000/api/workouts",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    setWorkouts(res.data);
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+  useEffect(() => {
+    fetchWorkouts();
+  }, []);
+
+  const handleDelete = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    await axios.delete(
+      `http://localhost:5000/api/workouts/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    alert("Workout Deleted 🗑️");
+
+    fetchWorkouts();
+
+  } catch (error) {
+    alert(error.response?.data?.message || "Delete Failed");
+  }
+};
+
+  const handleAddWorkout = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      await axios.post(
+        "http://localhost:5000/api/workouts",
+        {
+          exerciseName,
+          muscleGroup,
+          sets,
+          reps,
+          duration,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      alert("Workout Added Successfully 💪");
+      
+      fetchWorkouts();
+
+      setExerciseName("");
+      setMuscleGroup("");
+      setSets("");
+      setReps("");
+      setDuration("");
+
+    } catch (error) {
+      alert(error.response?.data?.message || "Something went wrong");
+    }
+  };
 
   return (
-    <div className="workouts-container">
-      <input className="search"
+    <div>
+      <h1>🏋️ Workouts</h1>
+
+      <input
         type="text"
-        placeholder="Search workout..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Exercise Name"
+        value={exerciseName}
+        onChange={(e) => setExerciseName(e.target.value)}
       />
 
-      {filteredWorkouts.map((workout) => (
-        <WorkoutCard
-          key={workout.id}
-          title={workout.title}
-          muscle={workout.muscle}
-        />
-      ))}
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Muscle Group"
+        value={muscleGroup}
+        onChange={(e) => setMuscleGroup(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="number"
+        placeholder="Sets"
+        value={sets}
+        onChange={(e) => setSets(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="number"
+        placeholder="Reps"
+        value={reps}
+        onChange={(e) => setReps(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="number"
+        placeholder="Duration"
+        value={duration}
+        onChange={(e) => setDuration(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleAddWorkout}>
+        Add Workout
+      </button>
+
+    <hr />
+
+<h2>My Workouts</h2>
+
+{workouts.map((workout) => (
+  <div
+    key={workout._id}
+    style={{
+      border: "1px solid white",
+      padding: "10px",
+      margin: "10px 0",
+      borderRadius: "10px",
+    }}
+  >
+    <h3>{workout.exerciseName}</h3>
+
+    <p>💪 {workout.muscleGroup}</p>
+
+    <p>
+      {workout.sets} Sets × {workout.reps} Reps
+    </p>
+
+    <p>⏱️ {workout.duration} mins</p>
+
+    <button
+  onClick={() => handleDelete(workout._id)}
+>
+  Delete 🗑️
+</button>
+  </div>
+))}  
+
+
     </div>
   );
 }
